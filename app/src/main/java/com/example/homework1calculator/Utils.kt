91d581223962bb66c658115eaedc1bbafadf4e38
Pavel Yushkevich <82846218+YushkevichP@users.noreply.github.com>
2022-04-13 12:51:29 +0300

    Revert "Hm#1 Yushkevich Pavel"

--- app/src/main/java/com/example/homework1calculator/Utils.kt
+++ app/src/main/java/com/example/homework1calculator/Utils.kt
@@ -1,14 +1,5 @@
 package com.example.homework1calculator
 
 
-val Char.isCorrectSymbol
-    get() = when (this) {
-        '*' -> true
-        '/' -> true
-        '+' -> true
-        '-' -> true
-        '.' -> true
-        '(' -> true
-        ')' -> true
-        else -> false
-    }
+
+
